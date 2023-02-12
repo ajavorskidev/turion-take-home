2b93344b59bd41b3bf698a7cb6f84a687e4033a3
Adrian Javorski <ajavorskidev@gmail.com>
2023-02-12 01:24:57 -0800

    Resolve issue of session error appearing when user registers a new account

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
-    devise_for :users, :skip => :sessions
+    devise_for :users
     resources :missions
     resources :images
   end
